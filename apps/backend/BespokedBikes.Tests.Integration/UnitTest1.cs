67b9c17552c050508e2c053d0b677c623be9cf73
bobthearsonist <bobthearsonist@users.noreply.github.com>
2025-11-08 14:07:42 -0500

    chore: cleanups

---
+++ apps/backend/BespokedBikes.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BespokedBikes.Tests.Integration;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
