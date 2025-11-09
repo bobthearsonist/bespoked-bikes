002c783ef606c6e34dc1775ffa468aff40c4e10f
bobthearsonist <bobthearsonist@users.noreply.github.com>
2025-11-09 10:38:44 -0500

    refactor: significantly reduce toil/code for mapping with assembly scanning and global type converters

--- apps/backend/BespokedBikes.Application/Features/Products/ProductDto.cs
+++ apps/backend/BespokedBikes.Application/Features/Products/ProductDto.cs
@@ -1,6 +1,10 @@
+using AutoMapper;
+using BespokedBikes.Domain.Entities;
+
 // ReSharper disable once CheckNamespace
 namespace BespokedBikes.Application.Generated;
 
+[AutoMap(typeof(Product), ReverseMap = true)]
 public partial class ProductDto
 {
     // Add any custom properties or methods here
